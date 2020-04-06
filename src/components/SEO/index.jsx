626a55404b52fae7f2556a76e2efab01821fb427
Alan Ray Ampersand <alan.ampersand@nike.com>
2020-04-05 22:03:28 -0700

    duplicate title bug fix

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -36,7 +36,6 @@ function SEO({ description, lang, meta, keywords, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
           name: 'description',
